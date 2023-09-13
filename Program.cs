39baa6d8380b4680f6c138c1cd37c36b8b2f865c
Le, Linh <linh.le@tecalliance.net>
2023-09-13 10:48:03 +0200

    second exercise

--- Program.cs
+++ Program.cs
@@ -7,20 +7,21 @@ namespace HelloWorld
         static void Main(string[] args)
         {
           //Frage den Nutzer nach der ersten Zahl
-          Console.WriteLine("Gebe mir der ersten Zahl");
-          string Zahl1 = Console.ReadLine();
+         Console.WriteLine("Gebe mir der ersten Zahl");
+         string eingabe1 = Console.ReadLine();
+          
 
           //Fragen den Nutzer nach der zweiten Zahl
           Console.WriteLine("Gebe mir der zweiten Zahl");
-          string Zahl2 = Console.ReadLine();
+          string eingabe2 = Console.ReadLine();
 
 
           //Gebe das Ergebnis auf der Console aus
-          //"Das Ergebnis aus {{Zahl1}} + {{Zahl2}} = {{Ergebnis}}
-          int Zahl1int = Convert.ToInt16(Zahl1);
-          int Zahl2int = Convert.ToInt16(Zahl2);
-          int Ergebnis = Zahl1int + Zahl2int;
-          Console.WriteLine($"Das Ergebnis aus {Zahl1int} + {Zahl2int} = {Ergebnis}");
+          //"Das Ergebnis aus {{Zahl1}} - {{Zahl2}} = {{Ergebnis}}
+          int Zahl1 = Convert.ToInt16(eingabe1);
+          int Zahl2 = Convert.ToInt16(eingabe2);
+          int Ergebnis = Zahl1 - Zahl2;
+          Console.WriteLine($"Das Ergebnis aus {Zahl1} - {Zahl2} = {Ergebnis}");
         }
 
     }
