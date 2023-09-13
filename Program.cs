dac0135f56a5134637e7463a181ed42645c8bc1b
Le, Linh <linh.le@tecalliance.net>
2023-09-13 15:04:35 +0200

    While Loop

--- Program.cs
+++ Program.cs
@@ -7,20 +7,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-           string[] emails = new string []
-        {
-            "linh.le@tecalliance.net",
-            "abc@tecalliance.net",
-            "qwertz@tecalliance.net"
-        };
-        foreach(string email in emails)
-        {
-            Console.WriteLine($"Die Email lautet: {email}");
+            List<int> myNumberList = new List<int>();
+            
+            Console.WriteLine("Sage mir wie viele Zahlen du in deine Liste hinzugügen möchtest");
+            int amountNumbers = Convert.ToInt32(Console.ReadLine());
+
+            while(myNumberList.Count < amountNumbers)
+            {
+                Console.WriteLine("Gebe mir eine Zahl");
+                myNumberList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            //Summiere alle zahlen auf
+            int ergebnis = 0;
+            foreach(int zahl in myNumberList)
+            {
+                ergebnis += zahl;
+            }
+            Console.WriteLine($"Das Ergebnis ist {ergebnis}");
+      
         }
-        for(int position=0; position < emails.Length; position++)
-        {
-            Console.WriteLine($"Die Email lautet:{emails[position]}");
+    
         }
-        }
-        }
+
     }
