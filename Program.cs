3c4b993ad7528566e49c246d6be7249d32b61143
Le, Linh <linh.le@tecalliance.net>
2023-09-13 09:22:10 +0200

    added code for learning C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Gebe mir deinen Namen!");
+           string name = Console.ReadLine();
+           
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # beginning
+This is an example project for learning C#.
+
+This is another information.
+
+## Heading 
+
+### Title
+abcd
+
+# Chapter 1
+
