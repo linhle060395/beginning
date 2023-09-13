44edeb8017d1d5d5fdaf1a7ddc173e6b15afb410
Le, Linh <linh.le@tecalliance.net>
2023-09-13 14:25:32 +0200

    name length

--- Program.cs
+++ Program.cs
@@ -7,35 +7,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int zahl1 = 600;
-            int zahl2 = 200;
+           Console.WriteLine("Gebe mir deinen Namen!");
+           string name = Console.ReadLine();
 
-            int ergebnis1 = zahl1 + zahl2;
-            Console.WriteLine(ergebnis1);
-
-            int ergebnis2 = zahl1 - zahl2;
-            Console.WriteLine(ergebnis2);
-
-            int ergebnis3 = zahl1 * zahl2;
-            Console.WriteLine(ergebnis3);
-
-            int ergebnis4 = zahl1 / zahl2;
-            Console.WriteLine(ergebnis4);
-
-            int ergebnis5 = zahl1 % zahl2;
-            Console.WriteLine(ergebnis5);
-
-            zahl1++;
-            Console.WriteLine(zahl1);
-
-            zahl2--;
-            Console.WriteLine(zahl2);
-
-            zahl1 *= 2; //zahl1 = zahl1 * 2;
-            Console.WriteLine(zahl1);
-
-            zahl2 += 25; //zahl2 = zahl2 + 25;
-            Console.WriteLine(zahl2);
+           if(name.Length > 5)
+           {
+                Console.WriteLine("Oh der Name ist aber lang");
+           }
+           else
+           {
+                Console.WriteLine("Der ist ja kurz");
+           }
         }
 
     }
