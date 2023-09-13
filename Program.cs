a478d3d1158d17dec1cd4652cd32cf23985b6bbc
Le, Linh <linh.le@tecalliance.net>
2023-09-13 14:32:17 +0200

    name length example 2

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace HelloWorld
            Console.WriteLine("Gebe mir deinen Namen!");
            string name = Console.ReadLine();
 
-           if(name.Length > 5)
+           if(name.Length > 10)
            {
                 Console.WriteLine("Oh der Name ist aber lang");
+           }
+           else if (name.Length > 5)
+           {
+                Console.WriteLine("Der Name ist mittellang");
            }
            else
            {
