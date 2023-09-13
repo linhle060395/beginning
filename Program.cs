cc3b9466af5e10f58c982c048953cabaf252c91f
Le, Linh <linh.le@tecalliance.net>
2023-09-13 14:39:04 +0200

    for-loop

--- Program.cs
+++ Program.cs
@@ -7,22 +7,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Gebe mir deinen Namen!");
-           string name = Console.ReadLine();
-
-           if(name.Length > 10)
-           {
-                Console.WriteLine("Oh der Name ist aber lang");
-           }
-           else if (name.Length > 5)
-           {
-                Console.WriteLine("Der Name ist mittellang");
-           }
-           else
-           {
-                Console.WriteLine("Der ist ja kurz");
-           }
+           for(int zaehlvariable=0; zaehlvariable<10; zaehlvariable++)
+        {
+            Console.WriteLine(zaehlvariable);
         }
+        }
+
 
     }
 }
