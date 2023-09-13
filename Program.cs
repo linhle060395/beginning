bf4c76cab299e45c239ef4231bc2fcbafe750120
Le, Linh <linh.le@tecalliance.net>
2023-09-13 10:34:46 +0200

    practice exercise 1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-﻿namespace HelloWorld
+﻿using System.Runtime.CompilerServices;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Gebe mir deinen Namen!");
-           string name = Console.ReadLine();
-           
-            Console.WriteLine("Hello, " + name);
+          //Frage den Nutzer nach der ersten Zahl
+          Console.WriteLine("Gebe mir der ersten Zahl");
+          string Zahl1 = Console.ReadLine();
+
+          //Fragen den Nutzer nach der zweiten Zahl
+          Console.WriteLine("Gebe mir der zweiten Zahl");
+          string Zahl2 = Console.ReadLine();
+
+
+          //Gebe das Ergebnis auf der Console aus
+          //"Das Ergebnis aus {{Zahl1}} + {{Zahl2}} = {{Ergebnis}}
+          int Zahl1int = Convert.ToInt16(Zahl1);
+          int Zahl2int = Convert.ToInt16(Zahl2);
+          int Ergebnis = Zahl1int + Zahl2int;
+          Console.WriteLine($"Das Ergebnis aus {Zahl1int} + {Zahl2int} = {Ergebnis}");
         }
+
     }
 }
