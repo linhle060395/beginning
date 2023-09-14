69ae39552c0348f8da47f780ab7bee4433f05720
Le, Linh <linh.le@tecalliance.net>
2023-09-14 10:07:05 +0200

    Aufgabe 3: Lasse den Nutzer eine Zahl eingeben und gebe alle Zahlen bis einschließlich der eingegeben Zahl aus

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace HelloWorld
@@ -7,16 +9,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe mir der ersten Nummer");
-            string Eingabe1 = Console.ReadLine();
+            Console.WriteLine("Gebe mir ein Zahl");
+            int Zahl = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("Gebe mir der zweiten Nummer");
-            string Eingabe2 = Console.ReadLine();
-
-            int Nummer1 = Convert.ToInt16(Eingabe1);
-            int Nummer2 = Convert.ToInt16(Eingabe2);
-            int Ergebnis = Nummer1 + Nummer2;
-            Console.WriteLine("Die Summe von 2 Nummer ist " + Ergebnis);
+            for(int i = 0; i <= Zahl; i++)
+            {
+                Console.WriteLine(i);
+            }            
         }
     }
 }
