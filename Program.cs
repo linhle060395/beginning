68409416f490e6b711ad8d4d4b4e031c0be241f4
Le, Linh <linh.le@tecalliance.net>
2023-09-14 20:10:27 +0200

    Aufgabe 5: Lasse den Nutzer eine Zahl eingeben und berechne von dieser Zahl die Fakultät.

--- Program.cs
+++ Program.cs
@@ -10,28 +10,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a number");
-            int number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            for(int i=1; i<=number; i++)
-            {
-                if(i%3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if(i%5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else if(i%3 == 0 && i%5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            int factorial = CalculateFactorial(number);
+
+            Console.WriteLine($"The factorial of {number} ist {factorial}");
+        }
+
+        static int CalculateFactorial(int n)
+        {
+            if(n == 0)
+                return 1;
+            else    
+                return n * CalculateFactorial(n - 1);
         }
     }
 }
