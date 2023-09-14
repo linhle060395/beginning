b2a91cf33832a7308975bb030562895a38bc41c1
Le, Linh <linh.le@tecalliance.net>
2023-09-14 08:17:08 +0200

    aufgabe1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus

--- Program.cs
+++ Program.cs
@@ -6,31 +6,12 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-{
-        //Frage den Nutzer bis zu welcher Zahl aufsummiert werden soll
-       
-        //Mache das in allen möglichen Schleifenarten
-        //In welcher geht es nicht und wieso?
-            //for, foreach, while, do while
-       
-       List<int> myNumberList = new List<int>();
-       
-       Console.WriteLine("Bis zu welcher Zahl soll aufsummiert werden?");
-       int untilNumber = Convert.ToInt32(Console.ReadLine());
+        {
+            int nummer1 = 25;
+            int nummer2 = 36;
 
-       int i = 0;
-       int zahl = 0;
-       int ergebnis = 0;
-
-       while(i<untilNumber)
-       {
-            zahl = i + 1;
-            ergebnis += zahl;
-            Console.WriteLine($"Added number {i}, current result {ergebnis}");
-            i++;
-       }       
-       
+            int ergebnis = nummer1 + nummer2;
+            Console.WriteLine(ergebnis);
+        }
     }
 }
-}
-            
