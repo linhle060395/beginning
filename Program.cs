97e8ce9f5d695e2bf4fdc8cfb5aca0271bd782f3
Le, Linh <linh.le@tecalliance.net>
2023-09-15 09:45:18 +0200

    numero & Addtion With Return

--- Program.cs
+++ Program.cs
@@ -21,13 +21,18 @@ namespace HelloWorld
 
             Console.WriteLine("Geschafft");
 
-            int zahl1 = 16;
-            int zahl2 = 32;
+            string zahl1 = Console.ReadLine();
+            string zahl2 = Console.ReadLine();
+
+            int numero = Convert.ToInt16(zahl2);
 
             switch(operators)
             {
                 case "+":
-                    Addition(zahl1, zahl2);
+                    string ergebnis = AdditionWithReturn(zahl1, zahl2).ToString();
+                    ergebnis = ergebnis.PadLeft(16,' ');
+                    Console.WriteLine("Das Ergebnis ist");
+                    Console.WriteLine(ergebnis);
                     break;
                 case "-":
                     Substract(zahl1, zahl2);
@@ -44,7 +49,37 @@ namespace HelloWorld
             }
         }
     
-
+        static int AdditionWithReturn(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            return numero1 + numero2;
+        }
+        static int SubstractWithReturn(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            return numero1 - numero2;
+        }
+        static int MultiplyWithReturn(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            return numero1 * numero2;
+        }
+        static int DivisionWithReturn(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            return numero1 / numero2;
+        }
+        static void Addition(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 + numero2);
+        }
+        
         static void Addition(int number1, int number2)
         {
             Console.WriteLine(number1 + number2);
@@ -64,6 +99,23 @@ namespace HelloWorld
         {
             Console.WriteLine(number1 / number2);
         }
-
+        static void Substract(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 - numero2);
+        }
+        static void Multiply(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 * numero2);
+        }
+        static void Division(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 / numero2);
+        }
     }
 }
