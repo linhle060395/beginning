2fbf9e20bcb70dae2294ac998aa1b36146226de4
Le, Linh <linh.le@tecalliance.net>
2023-09-25 16:01:42 +0200

    File hangdling exercises

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Immutable;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
 using System.Globalization;
 using System.Net.NetworkInformation;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
 using System.Transactions;
 using System.Xml.XPath;
 using Microsoft.VisualBasic;
@@ -32,50 +34,150 @@ namespace HelloWorld
            */
 
            // 1. Kopiere aus T:\WKH_EXC\Trash\cbec\ die Datei data.csv auf deine Festplatte C:\..... (Freie Wahl möglich)
-           string InitialPath = @"T:/WKH_EXC/Trash/cbec/data.csv";
-           string NewPath = @"C:/TraineeOffice/beginning/data.csv";
+           // 2. Kopiere die Datei unter einem anderen Namen
+           string filepath = @"T:/WKH_EXC/Trash/cbec/data.csv";
+           FileInfo fi = new FileInfo(filepath);
 
+           // fi.CopyTo("C:/TraineeOffice/beginning/copy_data.csv");
+
+
+           // 3. Lösche die kopierte Datei wieder
+           // FileInfo fileToDelete = new FileInfo("C:/TraineeOffice/beginning/copy_data.csv");
+           // fileToDelete.Delete();
            
 
-           try
-        {
-            File.Copy(InitialPath, NewPath, true);
-            Console.WriteLine("The file was copied successfully.");
-        }
-            catch (Exception ex)
-        {
-            Console.WriteLine("The file cannot be copied. " + ex.Message);
+           // 4. Kopiere die Datei in ein Variables Verzeichnis, welches der Nutzer selbst setzen kann
+           // Console.WriteLine("Wohin willst du die Datei kopieren?");
+           // string newpath = Console.ReadLine();
+           // fi.CopyTo(newpath);
+           // Wohin willst du die Datei kopieren?
+           //C:/TraineeOffice/beginning/abcd
 
-        }
-
-
-        // 2. Kopiere die Datei unter einem anderen Namen
-        string InitialPath2 = @"C:\TraineeOffice\beginning\die Datei data.csv";
-        string NewPath2 = @"C:\TraineeOffice\beginning\die neue Datei data.csv";
-
-        try
-        {
-            File.Copy(InitialPath2, NewPath2, true);
-            Console.WriteLine("The file name was changed successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("The file name cannot be changed. " + ex.Message);
-        }
+           // 5. Probiere eine Datei einzulesen, welche nicht existiert. Behalte die Lösung im Hinterkopf wenn du zukünftig Dateien einliest, damit du hier keinen Fehler bekommst!!!
+            /* if(fi.Exists)
+            { 
+                fi.CopyTo("C:/TraineeOffice/beginning/copy_data.csv");
+            }
+            else
+            {
+                Console.WriteLine("Diese Datei existiert nicht." + filepath);
+            }
+            */
 
 
-        // 3. Lösche die kopierte Datei wieder
-        try
-        {
-            File.Delete(NewPath2);
-            Console.WriteLine("The file was deleted successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("The file cannot be deleted. " + ex.Message);
-        }
+            // 6. Lese Datei mittels einem C#- Programm ein
+            /* if(fi.Exists)
+            {
+                StreamReader reader = new StreamReader(filepath, Encoding.UTF8);
+                
+                List<string> myEntries = new List<string> ();
+                while(!reader.EndOfStream)
+                {
+                    myEntries.Add(reader.ReadLine());
+                }
+                Console.WriteLine(myEntries[0]);
+                Console.WriteLine(myEntries.Count);
+            }
 
+            else
+            {
+                Console.WriteLine("Diese Datei existiert nicht." + filepath);
+            }
+            */
         
+            // 7. Teile die Einträge am Separator und gebe die ersten 10 Zeilen mit einem Tabulator separiert aus.
+            //  8. Gebe nur die Einträge aus, welche vom Alter jünger als 20 und männlich sind
+
+            //  9. Speichere diese in einer neuen CSV-Datei mit dem Namen „JungeHüpfer.csv“
+            /*if(fi.Exists)
+            {
+                StreamReader reader = new StreamReader(filepath, Encoding.UTF8);
+                
+                List<string[]> myEntries = new List<string[]> ();
+                while(!reader.EndOfStream)
+                {
+                    myEntries.Add(reader.ReadLine().Split(';'));
+                }
+                List<string[]> malesunder20 = myEntries
+                                                        .Where(w => w[0] == "m" && Convert.ToInt16(w[2]) < 20)
+                                                        .ToList();
+                
+                using(StreamWriter writer = new StreamWriter("C:/TraineeOffice/beginning/JungeHuepfer.csv", false, Encoding.UTF8))
+                {
+                    foreach(string [] entry in malesunder20)
+                    {
+                        writer.WriteLine(string.Join("\t", entry));
+                    }
+                }
+                */
+
+                // Console.WriteLine(myEntries.Where(w => w[0] == "m" && Convert.ToInt16(w[2]) < 20).Count()); //Count how many males < 20 years old
+
+                /*foreach(string[] entry in malesunder20)
+                {
+                    Console.WriteLine(string.Join("\t", entry));
+                }
+            }
+             else
+            {
+                Console.WriteLine("Diese Datei existiert nicht." + filepath);
+            }
+            */
+
+
+
+            // Aufgabe: baue die Zeile drüber in eine For-Each Schleife; mit If-Bedingung um das selbe zu erziehlen.
+            /* List<string[]> myEntries = new List<string[]> ();
+            foreach(string [] value in myEntries)
+            {
+                if (value [0] == "m" && Convert.ToInt32(value[2]) < 20)
+            {
+                Console.WriteLine(string.Join("\t", value));
+            }
+
+            else
+            {
+                Console.WriteLine("Diese Datei existiert nicht." + filepath);
+            }
+            }
+            */
+
+
+            // 10. Erstelle eine Schleife welche die Einträge nach Alter separieren. (Gewichte_18.csv, Gewichte_19.csv ……. Bis  Alter 100) 
+            if(fi.Exists)
+            {
+                StreamReader reader = new StreamReader(filepath, Encoding.UTF8);
+                
+                List<string[]> myEntries = new List<string[]> ();
+                string headerLine = reader.ReadLine();
+                while(!reader.EndOfStream)
+                {
+                    myEntries.Add(reader.ReadLine().Split(';'));
+                }
+                Console.WriteLine(string.Join("\t", myEntries[0]));
+                Console.WriteLine(myEntries.Count);   
+
+                int endAlter = 100;
+
+                for(int alter = 18; alter < endAlter; alter++)
+                {
+                       Console.WriteLine("Aktuell bin ich: " + alter);
+                
+                List<string[]> gefilterEintrage = myEntries
+                                                        .Where(w => Convert.ToInt16(w[2]) == alter)
+                                                        .ToList(); 
+                
+                using(StreamWriter writer = new StreamWriter("C:/TraineeOffice/beginning/Gewichte_{alter}.csv", true, Encoding.UTF8))
+                {
+                        foreach(string[] entry in gefilterEintrage)
+                {
+                    writer.WriteLine(string.Join('\t', entry));
+                }
+                }
+                }
+            }
+                
         }
     }
 }
+
